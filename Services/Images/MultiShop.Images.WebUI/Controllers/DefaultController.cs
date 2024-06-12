6365fcdc9186273fbeedf2e4f9ddd19fddd13fbb
AbdullahKus <62968246+abdks@users.noreply.github.com>
2024-06-12 14:03:56 +0300

    12/06/2024 -->1.Commit

---
+++ Services/Images/MultiShop.Images.WebUI/Controllers/DefaultController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.Images.WebUI.DAL.Entities;
+using MultiShop.Images.WebUI.Services;
+
+namespace MultiShop.Images.WebUI.Controllers
+{
+    public class DefaultController : Controller
+    {
+        private readonly ICloudStorageService _cloudStorageService;
+
+        public DefaultController(ICloudStorageService cloudStorageService)
+        {
+            _cloudStorageService = cloudStorageService;
+        }
+
+
+
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ImageDrive ımageDrive)
+        {
+           if(ımageDrive.Photo != null)
+            {
+                ımageDrive.SavedFileName = GenerateFileNameToSave(ımageDrive.Photo.FileName);
+                ımageDrive.SavedUrl = await _cloudStorageService.UploadFileAsync(ımageDrive.Photo, ımageDrive.SavedFileName);
+            }
+            return RedirectToAction("Index", "Default");
+        
+        }
+
+        private string? GenerateFileNameToSave(string incomingFileName)
+        {
+            var fileName = Path.GetFileNameWithoutExtension(incomingFileName);
+            var extension = Path.GetExtension(incomingFileName);
+            return $"{fileName}-{DateTime.Now.ToUniversalTime().ToString("yyyyMMddHHmmss")}{extension}";
+        }
+
+
+     
+       
+    }
+}
---
+++ Services/Images/MultiShop.Images.WebUI/Services/ICloudStorageService.cs
@@ -0,0 +1,11 @@
+﻿namespace MultiShop.Images.WebUI.Services
+{
+    public interface ICloudStorageService
+    {
+        Task<string> GetSignedUrlAsync(string fileNameToRead, int timeOutInMinutes = 30);
+        Task<string> UploadFileAsync(IFormFile fileToUpload, string fileNameToSave);
+        Task DeleteFileAsync(string fileNameToDelete);
+    }
+
+
+}
