36dca83ccda4005fd36e5c5f8d4771c31f04c961
AbdullahKus <Abdullahkus080@gmail.com>
2024-06-20 17:54:33 +0300

    20/06/2024 -->1.Commit

---
+++ RabbitMQMessage/MultiShop.RabbitMQMessageApi/Controllers/MessageController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace MultiShop.RabbitMQMessageApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CreateMessage()
+        {
+            var connectionFactory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+
+            };
+            var connection = connectionFactory.CreateConnection();
+            
+            var channel = connection.CreateModel();
+
+            channel.QueueDeclare("Kuyruk1", false, false, false, arguments: null);
+
+            var messageContent = "Merhaba bu bir rabbitmq kuyruk mesajıdır";
+            var byteMessageContent = Encoding.UTF8.GetBytes(messageContent);
+            channel.BasicPublish(exchange:"",routingKey:"Kuyruk1",basicProperties:null,body :byteMessageContent);
+
+            return Ok("Mesajınız Alınmıştır");
+        }
+
+        private static string message;
+        [HttpGet]
+        public IActionResult ReadMessage()
+        {
+            var factory = new ConnectionFactory();
+            
+            factory.HostName = "localhost";
+
+            var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += (model, x) =>
+            {
+                var byteMessage = x.Body.ToArray();
+                var message = Encoding.UTF8.GetString(byteMessage);
+                
+            };
+            channel.BasicConsume(queue:"Kuyruk1",autoAck:false,consumer:consumer);
+            if(string.IsNullOrEmpty(message))
+                return NoContent();
+            else
+                return Ok(message);
+        }
+
+    }
+}
